be68ce445d29b53030d7d1d2874741ef39595858
Brady-Conn <jbc5099@gmail.com>
2024-11-20 15:41:00 -0700

    added shutdown and move response handlers

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
     An Elixir wrapper for the stockfish chess engine.
 
 # TODO
-  - handle shutdown
-  - change calls to sync calls (wait for response)
-  - build response mechanism
+  - think about concurrency management
   - figure out what to do with stockfish src files
   - figure out hex publishing
-  - think about concurrency management, parent process will handle for now
 
--- lib/chesster/stock_fish.ex
+++ lib/chesster/stock_fish.ex
@@ -13,22 +13,40 @@ defmodule Chesster.StockFish do
     GenServer.cast(__MODULE__, {:send_move, new_position})
   end
 
+  def shutdown() do
+    GenServer.call(__MODULE__, :shutdown)
+  end
+
   def handle_continue(:start_stockfish, state) do
     port = Port.open({:spawn_executable, state.dir}, [:binary])
     state = Map.put(state, :port, port)
     {:noreply, state}
   end
 
+  def handle_continue(:is_ready, state) do
+    Port.command(state.port, "isready\n")
+    {:noreply, state}
+  end
+
+  def handle_continue(:go, state) do
+    Port.command(state.port, "go\n")
+    {:noreply, state}
+  end
+
   def handle_cast({:send_move, new_position}, state) do
     Port.command(state.port, "position fen #{new_position}\n")
-    {:noreply, state}
+    {:noreply, state, {:continue, :go}}
+  end
+
+  def handle_call(:shutdown, _from, state) do
+    Port.close(state.port)
+    {:stop, :normal, state}
   end
 
   def handle_info({_port, {:data, "id name Stockfish 17\nid author the Stockfish developers (see AUTHORS file)\n\noption name Debug Log File type string default <empty>\noption name NumaPolicy type string default auto\noption name Threads type spin default 1 min 1 max 1024\noption name Hash type spin default 16 min 1 max 33554432\noption name Clear Hash type button\noption name Ponder type check default false\noption name MultiPV type spin default 1 min 1 max 256\noption name Skill Level type spin default 20 min 0 max 20\noption name Move Overhead type spin default 10 min 0 max 5000\noption name nodestime type spin default 0 min 0 max 10000\noption name UCI_Chess960 type check default false\noption name UCI_LimitStrength type check default false\noption name UCI_Elo type spin default 1320 min 1320 max 3190\noption name UCI_ShowWDL type check default false\noption name SyzygyPath type string default <empty>\noption name SyzygyProbeDepth type spin default 1 min 1 max 100\noption name Syzygy50MoveRule type check default true\noption name SyzygyProbeLimit type spin default 7 min 0 max 7\noption name EvalFile type string default nn-1111cefa1111.nnue\noption name EvalFileSmall type string default nn-37f18f62d772.nnue\nuciok\n"}}, state) do
     IO.inspect("uciok")
     Port.command(state.port, "ucinewgame\n")
-    Port.command(state.port, "isready\n")
-    {:noreply, state}
+    {:noreply, state, {:continue, :is_ready}}
   end
 
   def handle_info({_port, {:data, "Stockfish 17 by the Stockfish developers (see AUTHORS file)\n"}}, state) do
@@ -48,6 +66,12 @@ defmodule Chesster.StockFish do
 
   def handle_info({_port, {:data, data}}, state) do
     IO.inspect(data)
+    regex = ~r/bestmove (\w+)/
+    is_best_move = Regex.match?(regex, data)
+    if is_best_move do
+      config = Application.get_env(:chesster, :config) || %{on_move: fn _d -> nil end}
+      config.on_move(data)
+    end
     {:noreply, state}
   end
 end
